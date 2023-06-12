8046a57f071dfdb4e4817e7864470822722418bf
Di3ea3e <ars456gg@gmail.com>
2023-06-12 09:09:30 +0300

    bug fix

--- Shooter/Assets/Scripts/WeaponScripts/Ammo.cs
+++ Shooter/Assets/Scripts/WeaponScripts/Ammo.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 public class Ammo : MonoBehaviour
 {
-    public float speed;
-    public float distance;
-    public int damage;
-    public GameObject bloodSplash;
-    public GameObject destroyEffect;
-    public LayerMask layerMask;
-    public int destroy;
+    // public float speed;
+    // public float distance;
+    // public int damage;
+    // public GameObject bloodSplash;
+    // public GameObject destroyEffect;
+    // public LayerMask layerMask;
+    // public int destroy;
     
-    void Update()
-    {
-        RaycastHit2D other = Physics2D.Raycast(transform.position, transform.up, distance);
-        if(other.collider !=null)
-        {
-            if (other.collider.CompareTag("Enemy"))
-            {
-               other.collider.GetComponent<Enemy>().TakeDamage(damage);
-                Destroy(gameObject);
-            }
+    // void Update()
+    // {
+    //     RaycastHit2D other = Physics2D.Raycast(transform.position, transform.up, distance);
+    //     if(other.collider !=null)
+    //     {
+    //         if (other.collider.CompareTag("Enemy"))
+    //         {
+    //            other.collider.GetComponent<Enemy>().TakeDamage(damage);
+    //             Destroy(gameObject);
+    //         }
 
-            if (other.collider.CompareTag("Wall"))
-            {
-                Destroy(gameObject);
-            }
-        }
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
-    }
+    //         if (other.collider.CompareTag("Wall"))
+    //         {
+    //             Destroy(gameObject);
+    //         }
+    //     }
+    //     transform.Translate(Vector2.up * speed * Time.deltaTime);
+    // }
 
-    void Destroy()
-    {
-        Instantiate(destroyEffect, transform.position, Quaternion.identity);
-        Instantiate(bloodSplash, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-    }
-    void DestroyTime()
-    {
-        Destroy(gameObject);
-    }
-    void Start()
-    {
-        Invoke("DestroyTime", destroy);
-    }
+    // void Destroy()
+    // {
+    //     Instantiate(destroyEffect, transform.position, Quaternion.identity);
+    //     Instantiate(bloodSplash, transform.position, Quaternion.identity);
+    //     Destroy(gameObject);
+    // }
+    // void DestroyTime()
+    // {
+    //     Destroy(gameObject);
+    // }
+    // void Start()
+    // {
+    //     Invoke("DestroyTime", destroy);
+    // }
 }
