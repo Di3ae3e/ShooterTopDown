b72ce36193b64dba9cca5328204020512ce59117
Di3ea3e <ars456gg@gmail.com>
2023-06-12 09:25:49 +0300

    bug fix 2

--- Shooter/Assets/Scripts/WeaponScripts/WeaponShoot.cs
+++ Shooter/Assets/Scripts/WeaponScripts/WeaponShoot.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class WeaponShoot : MonoBehaviour
 {
-    public GameObject projectile;
-    public Transform projectileTransform;
+    // public GameObject projectile;
+    // public Transform projectileTransform;
 
-    public float StartTimeFire;
-    private float TimeFire;
+    // public float StartTimeFire;
+    // private float TimeFire;
 
-    private AudioSource audioSource;
+    // private AudioSource audioSource;
 
-    void Start()
-    {
-        TimeFire = StartTimeFire;
-        audioSource = GetComponent<AudioSource>();
-    }
+    // void Start()
+    // {
+    //     TimeFire = StartTimeFire;
+    //     audioSource = GetComponent<AudioSource>();
+    // }
 
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.Mouse0))
-        {
-            if(TimeFire <= 0)
-            {
-                Instantiate(projectile, projectileTransform.position, transform.rotation);
-                audioSource.Play();
-                TimeFire = StartTimeFire;
-            } 
-            else
-            {
-                TimeFire -=Time.deltaTime;
-            }
-        }
-    }
+    // void Update()
+    // {
+    //     if(Input.GetKey(KeyCode.Mouse0))
+    //     {
+    //         if(TimeFire <= 0)
+    //         {
+    //             Instantiate(projectile, projectileTransform.position, transform.rotation);
+    //             audioSource.Play();
+    //             TimeFire = StartTimeFire;
+    //         } 
+    //         else
+    //         {
+    //             TimeFire -=Time.deltaTime;
+    //         }
+    //     }
+    // }
 }
